a2ef049a412ad2818608b1c8f09dcb55bf8fbf1f
Steplton7 <107725240+Steplton7@users.noreply.github.com>
2023-01-24 22:28:35 +0300

    Смена пароля(url reset/done complete)

--- auth_dj/users/urls.py
+++ auth_dj/users/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView,
     PasswordResetCompleteView
     )
-
+from django.urls.base import reverse_lazy
 from . import views
 
 app_name = 'users'
@@ -31,8 +31,10 @@ urlpatterns = [
     path(
         'password-reset/', 
         PasswordResetView.as_view(template_name='users/password_reset.html',
-        html_email_template_name='users/password_reset_email.html'),
+        html_email_template_name='users/password_reset_email.html',
+        success_url = reverse_lazy("users:password_reset_done")),
         name='password-reset'),
+
     path(
         'password-reset/done/',
         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
